0d184c73ce721d70787b9954120c572cfaaafd82
ekowi <greget103@gmail.com>
2024-01-01 00:40:00 +0700

    costum csrf allowed host

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -27,12 +27,12 @@ SECRET_KEY = "django-insecure-e_#(%l1d--)s^lg9-wk@&w54!@f=d6(4-aq+qja(*uce^kid5i
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "https://*.*.*.amazonaws.com",
+    "*.*.*.amazonaws.com",
     "127.0.0.1",
-    "https://*.*.amazonaws.com",
-    "https://*.amazonaws.com",
+    "*.*.amazonaws.com",
+    "*.amazonaws.com",
     "localhost",
-    "https://ks1dwmfvs7.execute-api.ap-southeast-1.amazonaws.com",
+    "ks1dwmfvs7.execute-api.ap-southeast-1.amazonaws.com",
 ]
 
 CSRF_TRUSTED_ORIGIN = [
