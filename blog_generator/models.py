cabcc62b56a9a7373bef508a8b0ab560b63527a1
ekowi <greget103@gmail.com>
2024-02-04 13:27:38 +0700

    add additionals

--- blog_generator/models.py
+++ blog_generator/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
+
+
 # Create your models here.
 class BlogPost(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -8,21 +10,18 @@ class BlogPost(models.Model):
     youtube_link = models.URLField()
     generated_content = models.TextField()
     created_at = models.DateField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.youtube_title
-    
+
+
 class Product(models.Model):
     """Model definition for Product."""
 
     title = models.CharField(max_length=255)
     description = models.TextField()
-    additionals = ArrayField(
-        models.CharField(max_length=255)
-    )
+    additionals = ArrayField(models.CharField(max_length=255), null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=255, null=True)
     url = models.URLField(null=True)
-    is_active = models.BooleanField()
-
-    
+    is_active = models.BooleanField(default=True)
