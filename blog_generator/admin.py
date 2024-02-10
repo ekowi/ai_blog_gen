bc53dfef841c67455ae230822adcda484e2eb20b
ekowi <greget103@gmail.com>
2024-02-11 02:37:38 +0700

    add many to many field

--- blog_generator/admin.py
+++ blog_generator/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import BlogPost
+from .models import BlogPost, Additionals, Product
 
 # Register your models here.
 admin.site.register(BlogPost)
+admin.site.register(Additionals)
+admin.site.register(Product)
--- blog_generator/models.py
+++ blog_generator/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.postgres.fields import ArrayField
+
+# from django.contrib.postgres.fields import ArrayField
 
 
 # Create your models here.
@@ -15,13 +16,25 @@ class BlogPost(models.Model):
         return self.youtube_title
 
 
+class Additionals(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Product(models.Model):
     """Model definition for Product."""
 
     title = models.CharField(max_length=255)
     description = models.TextField()
-    additionals = ArrayField(models.CharField(max_length=255), null=True, blank=True)
+    # additionals = ArrayField(models.CharField(max_length=255), null=True, blank=True)
+    additionals = models.ManyToManyField(Additionals)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=255, null=True)
     url = models.URLField(null=True)
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
