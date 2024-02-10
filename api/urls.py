a180b5256a2de225a051870677c612d963402301
ekowi <greget103@gmail.com>
2024-02-11 03:29:07 +0700

    add additionals api

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/product", views.get_all_product, name="api_product"),
-    path("/additionals", views.get_all_additionals, name="api_additionals"),
+    path("product", views.get_all_product, name="api_product"),
+    path("additionals", views.get_all_additionals, name="api_additionals"),
 ]
